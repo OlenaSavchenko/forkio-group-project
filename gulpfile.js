6227b081378cc91f851e276d3faf6c6b765e9d75
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-07-17 15:33:25 +0300

    remove exports of separate tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
-const { src, dest, watch, series, parallel} = require('gulp'); 
+const { src, dest, watch, series, parallel } = require('gulp');
 // Общие плагины
-const sourcemaps   = require('gulp-sourcemaps');        //source maps для дебага миниф.кода
-const concat       = require('gulp-concat');            //объединение файлов  
-const browserSync  = require('browser-sync').create();  //browserSync
-const del          = require('del');                    //удаление папок/файлов
-const rename       = require('gulp-rename');            //переименование
-const newer        = require('gulp-newer');             //проверка на то, были ли манипуляции с элементом ранее
+const sourcemaps = require('gulp-sourcemaps');         //source maps для дебага миниф.кода
+const concat = require('gulp-concat');                 //объединение файлов  
+const browserSync = require('browser-sync').create();  //browserSync
+const del = require('del');                            //удаление папок/файлов
+const rename = require('gulp-rename');                 //переименование
+const newer = require('gulp-newer');                   //проверка на то, были ли манипуляции с элементом ранее
 // Стили
-const sass         = require('gulp-sass');              //SCSS компилятор 
-const autoprefixer = require('gulp-autoprefixer');      //вендорные префиксы
-const cleancss     = require('gulp-clean-css');         //минификация стилей
-const purgecss     = require('gulp-purgecss')           //удаление неиспользуемого кода
-const ttfToWoff2   = require('gulp-ttf2woff2');         //конвертирование шрифтов в woff2
-const ttfToWoff    = require('gulp-ttf2woff');          //конвертирование шрифтов в woff
+const sass = require('gulp-sass');                     //SCSS компилятор 
+const autoprefixer = require('gulp-autoprefixer');     //вендорные префиксы
+const cleancss = require('gulp-clean-css');            //минификация стилей
+const purgecss = require('gulp-purgecss')              //удаление неиспользуемого кода
+const ttfToWoff2 = require('gulp-ttf2woff2');          //конвертирование шрифтов в woff2
+const ttfToWoff = require('gulp-ttf2woff');            //конвертирование шрифтов в woff
 // Изображения
-const imageMin     = require('gulp-imagemin');          //сжатие картинок
+const imageMin = require('gulp-imagemin');             //сжатие картинок
 // Скрипты
-const uglify       = require('gulp-uglify-es').default; //сжатие js
+const uglify = require('gulp-uglify-es').default;      //сжатие js
 //Html
-const fileInclude  = require('gulp-file-include');      //импорт html файлов в главный html
+const fileInclude = require('gulp-file-include');      //импорт html файлов в главный html
 
 
 // Html страницы
-function combineHtml(){
+function combineHtml() {
    return src('src/html/index.html')
-   .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(dest('./'))
-    .pipe(browserSync.stream()) 
-    
+      .pipe(fileInclude({
+         prefix: '@@',
+         basepath: '@file'
+      }))
+      .pipe(dest('./'))
+      .pipe(browserSync.stream())
+
 }
 
 
 // Конвертирование шрифтов
 function convertFonts() {
-      src('src/fonts/*.ttf')
+   src('src/fonts/*.ttf')
       .pipe(ttfToWoff2())
       .pipe(dest('dist/fonts/'))
 
@@ -49,51 +49,51 @@ function convertFonts() {
 // Манипуляция с SCSS для дев версии с sourcemaps
 function compilStylesDev() {
    return src('./src/scss/main.scss')
-      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sass().on('error', sass.logError))
-      .pipe(rename({suffix: '.min'}))
+      .pipe(rename({ suffix: '.min' }))
       .pipe(autoprefixer({ overrideBrowserslist: ['last 3 versions'] }))
       .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
       .pipe(sourcemaps.write())
       .pipe(dest('./dist/css/'))
-      .pipe(browserSync.stream()) 
+      .pipe(browserSync.stream())
 }
 
 // Манипуляция с SCSS для продакшн версии без sourcemaps и c очисткой неиспользуемых стилей
 function compilStylesProd() {
    return src('./src/scss/main.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(rename({suffix: '.min'}))
+      .pipe(rename({ suffix: '.min' }))
       .pipe(autoprefixer({ overrideBrowserslist: ['last 3 versions'] }))
       .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
-      .pipe(purgecss({ content: ['index.html', 'src/js/**/*.js']}))
+      .pipe(purgecss({ content: ['index.html', 'src/js/**/*.js'] }))
       .pipe(dest('./dist/css/'))
 }
 
 // Сжатие картинок(c пропуском уже сжатых)
-function compressImgs(){
+function compressImgs() {
    return src('src/img/**/*')
-   .pipe(newer('dist/img/'))
-   .pipe(imageMin())
-   .pipe(dest('dist/img/'));
+      .pipe(newer('dist/img/'))
+      .pipe(imageMin())
+      .pipe(dest('dist/img/'));
 }
 
 // Манипуляции с JS для дев версии с sourcemaps
-function scriptsDev(){
+function scriptsDev() {
    return src('src/js/**/*.js')
-   .pipe(sourcemaps.init())
-   .pipe(concat('script.min.js'))
-   .pipe(uglify())
-   .pipe(sourcemaps.write())
-   .pipe(dest('dist/js/'))
-   .pipe(browserSync.stream())
+      .pipe(sourcemaps.init())
+      .pipe(concat('script.min.js'))
+      .pipe(uglify())
+      .pipe(sourcemaps.write())
+      .pipe(dest('dist/js/'))
+      .pipe(browserSync.stream())
 }
 // Манипуляции с JS для продакшн версии без sourcemaps
-function scriptsProd(){
+function scriptsProd() {
    return src('src/js/**/*.js')
-   .pipe(concat('script.min.js'))
-   .pipe(uglify())
-   .pipe(dest('dist/js/'))
+      .pipe(concat('script.min.js'))
+      .pipe(uglify())
+      .pipe(dest('dist/js/'))
 }
 
 // Настройки Browsersync
@@ -107,7 +107,7 @@ function browserChanges() {
 
 // Очищение папки dist
 function cleanDist() {
-	return del('dist/**/*');
+   return del('dist/**/*');
 }
 
 // Отслеживание изменений
@@ -120,17 +120,5 @@ function startWatch() {
 }
 
 
-exports.convertFonts      = convertFonts; 
-exports.compilStylesDev   = compilStylesDev; 
-exports.compilStylesProd  = compilStylesProd; 
-exports.scriptsDev        = scriptsDev;      
-exports.scriptsProd       = scriptsProd;      
-exports.startWatch        = startWatch;   
-exports.browserChanges    = browserChanges;
-exports.cleanDist         = cleanDist;     
-exports.compressImgs      = compressImgs;
-exports.combineHtml       = combineHtml;
-
-
-exports.dev       = parallel(combineHtml, compilStylesDev, scriptsDev, compressImgs, convertFonts, browserChanges, startWatch);    //Для дев версия
-exports.build     = series(cleanDist, combineHtml, compilStylesProd, scriptsProd, compressImgs, convertFonts);                     //Для прод версии
+exports.dev = parallel(combineHtml, compilStylesDev, scriptsDev, compressImgs, convertFonts, browserChanges, startWatch);    //Для дев версия
+exports.build = series(cleanDist, combineHtml, compilStylesProd, scriptsProd, compressImgs, convertFonts);                     //Для прод версии
